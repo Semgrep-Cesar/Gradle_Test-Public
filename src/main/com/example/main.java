d74742f0daa10f2ab7ef73f0849e293b5e408842
David Whitlow <david.whitlow@r2c.dev>
2023-08-04 16:39:49 -0700

    init

---
+++ src/main/com/example/main.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import com.google.common.collect.Lists;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Using Google Guava library to create a list:");
+        System.out.println(Lists.newArrayList(1, 2, 3, 4, 5));
+    }
+}
